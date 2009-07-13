22405094c94689642c4fa2ad56bdaac3f9e8d7d5
ivank <ivank@ludios.net>
2009-07-13 00:00:45 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Webmagic',
+	version='9.7.13',
+	description="Web-related things",
+	packages=['webmagic', 'webmagic.test'],
+)

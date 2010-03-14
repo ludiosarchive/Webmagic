b3e2c1fd9199619c873804abe31889d4ec1b62d4
ivank <ivank@ludios.net>
2010-03-14 11:36:00 -0700

    Expose a __version__

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 
 from distutils.core import setup
 
+import webmagic
+
 setup(
 	name='Webmagic',
-	version='9.12.26',
+	version=webmagic.__version__,
 	description="Web-related things",
 	packages=['webmagic', 'webmagic.test'],
 )

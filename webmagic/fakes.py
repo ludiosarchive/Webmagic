c1c1f057baa68663448a313201a037d8f310c58f
ivank <ivank@ludios.net>
2010-11-18 08:17:32 +0000

    Add webmagic/fakes.py, including many fakes from minerva.mocks

---
+++ webmagic/fakes.py
@@ -0,0 +1,199 @@
+"""
+Better fakes than those that come with Twisted.
+"""
+
+from zope.interface import implements
+
+from twisted.internet import address, interfaces, task
+
+from twisted.web import server, resource
+from twisted.web.test.test_web import DummyRequest as _TwistedDummyRequest
+from twisted.test.proto_helpers import StringTransport
+#from twisted.internet.test.test_base import FakeReactor as _TwistedFakeReactor
+
+
+# The use of "mock" and "dummy" in this file is totally inconsistent.
+
+
+class GetNewMixin(object):
+
+	def getNew(self):
+		"""
+		Returns new log entries. This makes test code a lot less redundant.
+		"""
+		if not hasattr(self, '_returnNext'):
+			self._returnNext = 0
+
+		old = self._returnNext
+		self._returnNext = len(self.log)
+
+		return self.log[old:]
+
+
+
+class DumbLog(GetNewMixin):
+
+	def __init__(self):
+		self.log = []
+
+
+	def append(self, item):
+		return self.log.append(item)
+
+
+	def extend(self, items):
+		return self.log.extend(items)
+
+
+
+class FakeReactor(GetNewMixin):
+	# TODO	: implements() IReactorCore interface? or whatever addSystemEventTrigger is part of?
+
+	def __init__(self, *args, **kargs):
+		self.log = []
+
+
+	def addSystemEventTrigger(self, *args):
+		self.log.append(['addSystemEventTrigger'] + list(args))
+
+
+
+class DummyTCPTransport(StringTransport):
+
+	def __init__(self, *args, **kwargs):
+		StringTransport.__init__(self, *args, **kwargs)
+
+
+	def unregisterProducer(self):
+		"""
+		StringTransport does some weird stuff.
+
+		Real twisted code doesn't raise RuntimeError if no producer is registered.
+		Real twisted code doesn't set <streamingTransportVar> = None
+		"""
+		self.producer = None
+
+
+
+# copy/paste from twisted.web.test.test_web, but added a setTcpNoDelay
+class DummyChannel(object):
+	requestIsDone = False
+
+	# TODO: probably use DummyTCPTransport instead of this less-featured `class TCP'
+	class TCP(object):
+		port = 80
+		socket = None
+		connectionLostReason = None
+
+		def __init__(self):
+			self.noDelayEnabled = False
+			self.written = ''
+			self.producers = []
+			self.paused = 0
+
+		def getPeer(self):
+			return address.IPv4Address("TCP", '192.168.1.1', 12344)
+
+		def write(self, bytes):
+			assert isinstance(bytes, str)
+			self.written += bytes
+
+		def writeSequence(self, iovec):
+			for v in iovec:
+				self.write(v)
+
+		def getHost(self):
+			return address.IPv4Address("TCP", '10.0.0.1', self.port)
+
+		def pauseProducing(self):
+			self.paused += 1
+
+		def resumeProducing(self):
+			self.paused -= 1
+
+		def registerProducer(self, producer, streaming):
+			self.producers.append((producer, streaming))
+
+		def setTcpNoDelay(self, enabled):
+			self.noDelayEnabled = bool(enabled)
+
+		def connectionLost(self, reason):
+			self.connectionLostReason = reason
+
+
+	class SSL(TCP):
+		implements(interfaces.ISSLTransport)
+
+
+	def __init__(self, clock=None):
+		if clock is None:
+			clock = task.Clock()
+		self.site = server.Site(resource.Resource(), clock=clock)
+		self.transport = self.TCP()
+
+
+	def requestDone(self, request):
+		self.requestIsDone = True
+
+
+
+class DummyRequest(_TwistedDummyRequest):
+
+	def __init__(self, *args, **kwargs):
+		_TwistedDummyRequest.__init__(self, *args, **kwargs)
+
+		self.startedWriting = False
+
+		# This is needed because _BaseHTTPTransport does
+		#     self.request.channel.transport.setTcpNoDelay(True)
+		self.channel = DummyChannel()
+
+		self.received_cookies = {}
+
+
+	def write(self, data):
+		self.startedWriting = True
+		_TwistedDummyRequest.write(self, data)
+
+
+	def setHeader(self, name, value):
+		"""
+		L{twisted.web.test.test_web.DummyRequest} does strange stuff in
+		C{setHeader} -- it modifies self.outgoingHeaders, which is not close
+		enough to reality.
+		"""
+		self.responseHeaders.setRawHeaders(name, [value])
+
+
+	def isSecure(self):
+		return False
+
+
+	def getCookie(self, name):
+		return self.received_cookies.get(name)
+
+
+
+class MockProducer(GetNewMixin):
+	resumed = False
+	stopped = False
+	paused = False
+
+	def __init__(self):
+		self.log = []
+
+
+	def resumeProducing(self):
+		self.log.append(['resumeProducing'])
+		self.resumed = True
+		self.paused = False
+
+
+	def pauseProducing(self):
+		self.log.append(['pauseProducing'])
+		self.paused = True
+
+
+	def stopProducing(self):
+		self.log.append(['stopProducing'])
+		self.stopped = True

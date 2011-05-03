3ee8fe3a40700e82ffee7e35b63dccef158a4052
ivank <ivan@ludios.org>
2011-05-03 09:56:44 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ import webmagic
 setup(
 	name='Webmagic',
 	version=webmagic.__version__,
-	description="Web-related things",
+	description="A collection of Twisted and twisted.web-related utilities.",
+	url="https://github.com/ludios/Webmagic",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 3 - Alpha',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	packages=['webmagic', 'webmagic.test'],
 )

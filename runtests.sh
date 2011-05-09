818c545a54582c2f1c97eebdeb83a22baf3eca35
Ivan Kozik <ivan@ludios.org>
2011-05-09 08:47:26 +0000

    runtests.sh: Use correct REFBINDER env variable

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -e
 
-MYPY_REFBINDER_AUTOENABLE=0 time python     -W all `which trial` webmagic
-MYPY_REFBINDER_AUTOENABLE=1 time python     -W all `which trial` webmagic
-MYPY_REFBINDER_AUTOENABLE=0 time python -O  -W all `which trial` webmagic
-MYPY_REFBINDER_AUTOENABLE=1 time python -O  -W all `which trial` webmagic
-MYPY_REFBINDER_AUTOENABLE=0 time python -OO -W all `which trial` webmagic
-MYPY_REFBINDER_AUTOENABLE=1 time python -OO -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=0 time python     -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=1 time python     -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=0 time python -O  -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=1 time python -O  -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=0 time python -OO -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=1 time python -OO -W all `which trial` webmagic
 
-MYPY_REFBINDER_AUTOENABLE=0 time pypy       -W all `which trial` webmagic
-MYPY_REFBINDER_AUTOENABLE=1 time pypy       -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=0 time pypy       -W all `which trial` webmagic
+REFBINDER_AUTOENABLE=1 time pypy       -W all `which trial` webmagic
 
-#MYPY_REFBINDER_AUTOENABLE=0 time jython      `which trial` webmagic
-#MYPY_REFBINDER_AUTOENABLE=1 time jython      `which trial` webmagic
+#REFBINDER_AUTOENABLE=0 time jython      `which trial` webmagic
+#REFBINDER_AUTOENABLE=1 time jython      `which trial` webmagic

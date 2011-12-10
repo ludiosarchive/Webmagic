5054acc35017324728f97df1316c05f87b17cb93
Ivan Kozik <ivan@ludios.org>
2011-12-10 08:00:14 +0000

    Add Office 2007 mimetypes.  Python 2.7 mimetypes.py doesn't seem to include them.

--- webmagic/untwist.py
+++ webmagic/untwist.py
@@ -322,6 +322,33 @@ def loadCompatibleMimeTypes():
 		'.ico': 'image/x-icon',
 		'.log': 'text/plain',
 		'.rst': 'text/plain',
+
+		# Office 2007 formats
+		# http://technet.microsoft.com/en-us/library/ee309278%28office.12%29.aspx
+		'.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+		'.docm': 'application/vnd.ms-word.document.macroEnabled.12',
+		'.dotx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+		'.dotm': 'application/vnd.ms-word.template.macroEnabled.12',
+		'.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+		'.xlsm': 'application/vnd.ms-excel.sheet.macroEnabled.12',
+		'.xltx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+		'.xltm': 'application/vnd.ms-excel.template.macroEnabled.12',
+		'.xlsb': 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+		'.xlam': 'application/vnd.ms-excel.addin.macroEnabled.12',
+		'.pptx': 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+		'.pptm': 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+		'.ppsx': 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+		'.ppsm': 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+		'.potx': 'application/vnd.openxmlformats-officedocument.presentationml.template',
+		'.potm': 'application/vnd.ms-powerpoint.template.macroEnabled.12',
+		'.ppam': 'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+		'.sldx': 'application/vnd.openxmlformats-officedocument.presentationml.slide',
+		'.sldm': 'application/vnd.ms-powerpoint.slide.macroEnabled.12',
+		'.one': 'application/onenote',
+		'.onetoc2': 'application/onenote',
+		'.onetmp': 'application/onenote',
+		'.onepkg': 'application/onenote',
+		'.thmx': 'application/vnd.ms-officetheme',
 	})
 	return contentTypes
 
